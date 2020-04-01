1ffa3c15ecb4c093d7d46702e13c448f8f21220c
Suyash Kumar <suyashkumar2005@gmail.com>
2020-04-01 21:38:41 +0530

    added signup functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.admin import User
+from django.contrib import auth
 
 
 def signup(request):
 
-    return render(request, 'accounts/signup.html')
+    if (request.method == 'POST'):
+        # User wants an account now
+
+        if (request.POST['password1'] == request.POST['password2']):
+
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'Username already exists'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Passwords do not match'})
+
+    else:       
+        return render(request, 'accounts/signup.html')
 
 
 def login(request):
